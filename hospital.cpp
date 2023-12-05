093a06868464d7b995978e0ad96fdd744c4895d2
Pierre Dubois <152968677+scarcer-pensive@users.noreply.github.com>
2023-12-05 20:27:33 +0700

    Create  hospital.cpp

---
+++ hospital.cpp
@@ -0,0 +1,13 @@
+// hospital.cpp
+#include "hospital.h"
+#include <iostream>
+
+Hospital::Hospital() {
+    // Initialize any data members or setup here
+}
+
+void Hospital::run() {
+    // Implement the main logic of the hospital system here
+    std::cout << "Hospital Management System\n";
+    // Add more functionality as needed
+}
