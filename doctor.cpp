44d00935ee7b128a6421124366be2e35ae21cdc8
Pierre Dubois <152968677+scarcer-pensive@users.noreply.github.com>
2023-12-05 20:28:23 +0700

    Create doctor.cpp

---
+++ doctor.cpp
@@ -0,0 +1,6 @@
+// doctor.cpp
+#include "doctor.h"
+
+Doctor::Doctor(const std::string& name, const std::string& specialization) : name(name), specialization(specialization) {
+    // Initialize any data members or setup here
+}
